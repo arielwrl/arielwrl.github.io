36ea3754cb990b4bf7f5ba444d2d86e6bbbfc397
Ariel Werle <ariel.werle@gmail.com>
2025-05-29 15:04:58 +0200

    Trying eleventy deploy

--- .eleventy.js
+++ .eleventy.js
@@ -15,3 +15,14 @@ module.exports = function(eleventyConfig) {
     }
   };
 };
+
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+
+module.exports = function (eleventyConfig) {
+  // necessary to automatically prepend the prefix to internal links
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  return {
+    // prefix used by GitHub Pages, usually your project name
+    pathPrefix: "arielwrl.github.io"
+  }
+};
