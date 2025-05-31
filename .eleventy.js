dd3ae21023a3934fc4408174d619cc033d03e85b
Ariel Werle <ariel.werle@gmail.com>
2025-05-31 22:03:09 +0200

    Fixed MNRAS links

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,25 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-const { DateTime } = require("luxon");
+const { DateTime } = require("luxon"); // This library allows custom date formating
 
 module.exports = function(eleventyConfig) {
-  // Copy static assets like PDF, images, etc.
+  // Copy static assets like PDF, images, etc:
   eleventyConfig.addPassthroughCopy("assets");
-  // Copy styles.
+  // Copy styles:
   eleventyConfig.addPassthroughCopy("styles");
   // For github actions:
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-  // To format dates in the blog
+  // To format dates in the blog:
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("DDDD");
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("GG DDDD");
   });
-  // Collect markdown posts
+  // Collect markdown posts:
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("./posts/*.md");
   });
 
   return {
     dir: {
-      input: ".",        // current directory
+      input: ".",
       includes: "_includes",
       output: "_site",
       pathPrefix: "arielwrl.github.io"
