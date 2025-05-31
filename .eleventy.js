ab82f4222e9c46cd0ff251e091cb03ff3b30db20
Ariel Werle <ariel.werle@gmail.com>
2025-05-31 21:49:28 +0200

    Organizing style

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,17 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
   // Copy static assets like PDF, images, etc.
   eleventyConfig.addPassthroughCopy("assets");
+  // Copy styles.
+  eleventyConfig.addPassthroughCopy("styles");
   // For github actions:
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+  // To format dates in the blog
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("DDDD");
+  });
   // Collect markdown posts
   eleventyConfig.addCollection("posts", function(collectionApi) {
     return collectionApi.getFilteredByGlob("./posts/*.md");
